00906ab480bd9fbb97657937ec18b4393c5ab308
monkeysh1ne <mcdoob@gmail.com>
2018-01-21 14:09:48 +1300

    styled submit button.  added firebase connection and storage for newsletter subs

---
+++ js/main.js
@@ -0,0 +1,43 @@
+// Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyAs1NSzCyZeNJi25jz5J9DcL1U0eWtZp0I",
+    authDomain: "acmecontact-f887c.firebaseapp.com",
+    databaseURL: "https://acmecontact-f887c.firebaseio.com",
+    projectId: "acmecontact-f887c",
+    storageBucket: "acmecontact-f887c.appspot.com",
+    messagingSenderId: "662460980818"
+  };
+  firebase.initializeApp(config);
+
+
+// Reference messages collection
+var messagesRef = firebase.database().ref('messages');
+
+
+// Listen for form submit
+document.getElementById('newsletterSubrscibeForm').addEventListener('submit', submitForm);
+
+function submitForm(e){
+	e.preventDefault(); //don't submit to calling html page
+
+	// Get values
+	var email = getInputVal('email');
+
+	// Save message
+	saveMessage(email);
+
+
+}
+
+// Function to get form values
+function getInputVal(id){
+	return document.getElementById(id).value;
+}
+
+// Save message to firebase
+function saveMessage(email){
+	var newMessageRef = messagesRef.push();
+	newMessageRef.set({
+		email: email 
+	});
+}
